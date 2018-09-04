d2dc4a909ad55429484058905d8f92b90373717d
Hedi Sabah <headesabah@gmail.com>
2018-09-04 08:55:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let driver ={}
 
 function updateDriverWithKeyAndValue(driver, key, value){
   return Object.assign({}, driver, { [key]: value });
-  
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
